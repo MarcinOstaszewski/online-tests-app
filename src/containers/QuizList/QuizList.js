7c3d5f8d72be441ce72ec6d9aeb4559168e71b27
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-07-18 21:07:46 +0200

    add display of list of quizes

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { fbDB } from '../../firebase';
+
+import style from './QuizList.module.css';
+
+class QuizList extends Component {
+    state = { list: null }
+
+    componentDidMount() {
+        fbDB.ref('quizGroups/basicQuizes/names').once('value').then(snap => {
+            const quizes = Object.keys(snap.val())
+            console.log(quizes)
+            this.setState({
+                quizes: quizes
+            })
+        })
+    }
+
+    render() { 
+
+        let quizes;
+        if (this.state.quizes) {
+            quizes =  this.state.quizes.map( (q, i) => {
+            return <Link
+                    to={{
+                        pathname: `solve-quiz/`,
+                        hash: `${q.replace(/ /g,'-')}`,
+                        search: "3",
+                    }} 
+                key={i}
+                // onClick={this.props.chooseQuiz}
+                
+                >{q}</Link>
+                    } )
+        } 
+
+        return ( <div className={style.listElement}>
+            <h1>Choose a quiz for you</h1>
+            <div >
+                {quizes}
+            </div>
+        </div> );
+    }
+}
+ 
+export default QuizList;
+
