7139f3d7318104c22017598e0f0b92165cc4822f
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-07-18 21:04:58 +0200

    add Routes to index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Header from './components/Header/Header';
+import QuizList from './containers/QuizList/QuizList';
+import Quiz from './containers/Quiz/Quiz';
+import AddQuizData from './components/AddQuizData/AddQuizData';
+
+import { BrowserRouter, Route} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Route path="/add-quiz" component={AddQuizData} />
+      <Route path="/solve-quiz" component={Quiz} />
+      <Route path="/" exact component={QuizList} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
