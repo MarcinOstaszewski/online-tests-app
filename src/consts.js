08407a3a2990bba639dde17faa6000869d2ddd3f
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-07-30 23:00:42 +0200

    improve quize JSON structure

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,63 @@
+const quiz0001 = {
+    quizName: "Co wiesz o Celice?",
+    quizDescription: "Ten test sprawdzi Twoją wiedzę na temat historii rozwoju Toyoty Celiki.",
+    quizData: {
+        q1: {
+            questionText: "Kiedy zbudowano pierwszą Celicę?",
+            answerOptions: {
+                a1: "W roku 1444.",
+                a2: "12 kwietnia.",
+                a3: "O 12:33.",
+                a4: "Po podwieczorku.",
+            },
+            correctAnswer: 'a2'
+        },
+        q2: {
+            questionText: "W której generacji Celiki pojawiła się odmiara Celica Supra?",
+            answerOptions: {
+                a1: "I.",
+                a2: "XXI.",
+                a3: "II.",
+                a4: "V."
+            },
+            correctAnswer: 'a3'
+        },
+        q3: {
+            questionText: "Ile kół ma Celica?",
+            answerOptions: {
+                a1: "4",
+                a2: "2",
+                a3: "8",
+                a4: "Wszystkie!"
+            },
+            correctAnswer: 'a1'
+        },
+        q4: {
+            questionText: "Którędy Celica?",
+            answerOptions: {
+                a1: "Nie znam się, ja tu tylko sprzątam.",
+                a2: "Ja nie brałem!",
+                a3: "Ale o ssso chozzi?",
+                a4: "Nie mamy pańskiej Celiki i co nam pan zrobi?!",
+            },
+            correctAnswer: 'a4'
+        }
+    },
+    summary: {
+        summaryText: "Koniec kłiza nareszcie!",
+        resultMessages: {
+            0: "Tragedia baranie!",
+            25: "Ledwo-ledwo.",
+            50: "Coś tam wiesz...",
+            75: "Nie najgorzej!",
+            99: "REWELACJA ! ! !"
+        }
+
+    }
+}
+
+
+
 const basicSample01 = {
     quizName: "Elementary Questions",
     questions: {
@@ -79,6 +139,7 @@ const basic2 = {
 
 export {
     // quizGroups,
+    quiz0001,
     basicSample01,
     basicSample02,
     basic2
